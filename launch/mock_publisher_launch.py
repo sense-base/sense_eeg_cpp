ee6c04fb5593200f6e0a727024e81b3fab96eb67
nikk-nikaznan <nik.k.nikaznan@outlook.com>
2025-07-01 17:07:56 +0100

    add config file to the mock@

--- launch/mock_publisher_launch.py
+++ launch/mock_publisher_launch.py
@@ -1,12 +1,14 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     return LaunchDescription([
         Node(
             package='eeg_publisher_cpp',
             executable='mock_eeg_publisher',
             name='mock_eeg_publisher',
+            parameters=["config/mock_publisher.yaml"],
             output='screen'
         ),
     ])
